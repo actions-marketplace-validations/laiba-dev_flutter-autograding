947f35b74e5b2cf19125d1fd9b5b1bcd3dc4a6bb
mirfanrafif17 <mirfanrafif17@gmail.com>
2021-11-09 07:32:53 +0700

    mengambil test result

--- main.py
+++ main.py
@@ -10,11 +10,27 @@ def get_test(result_list):
         if(row.startswith('{"test":{')):
             listTest.append(json.loads(row))
     return listTest
+
+def remove_loading_test(test_list_with_description):
+    result = []
+
+    for item in test_list_with_description:
+        if item['test']['name'].startswith('loading'):
+            continue
+        else:
+            result.append(item)
+
+    return result
     
     
 def main():
+    # mengambil file
     result_list = get_file() 
-    print(get_test(result_list))
+    #mengambil data untuk test dan deskripsinya
+    test_list_with_description = get_test(result_list)
+    # menghilangkan test yang hanya untuk loading file
+    cleaned_test_list = remove_loading_test(test_list_with_description)
 
+    print(cleaned_test_list)
 
 main()
