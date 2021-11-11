c6a9a6ddd87522593139c30cff847dde993f428d
mirfanrafif17 <mirfanrafif17@gmail.com>
2021-11-11 10:14:33 +0700

    typo gan

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ cp /github/workspace/test-result.txt /app/flutter-autograding/test-result.txt
 
 ls /app/flutter-autograding
 
-python3 /app/flutter-autograding/main.py /app/flutter-autograding/text-result.txt $GITHUB_ACTOR $GITHUB_REPOSITORY $GITHUB_ACTION
+python3 /app/flutter-autograding/main.py /app/flutter-autograding/test-result.txt $GITHUB_ACTOR $GITHUB_REPOSITORY $GITHUB_ACTION
