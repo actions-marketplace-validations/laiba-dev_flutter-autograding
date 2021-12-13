9b68105278309b7e84e6a3153e4c1a1ab2589fe4
mirfanrafif17 <mirfanrafif17@gmail.com>
2021-12-13 09:14:20 +0700

    all on docker

--- github_entrypoint.sh
+++ github_entrypoint.sh
@@ -1,13 +1,17 @@
 #!/bin/sh -l
 
-echo $GITHUB_WORKSPACE
+#config flutter path
+export PATH="$PATH":"$HOME/.pub-cache/bin"
+flutter pub get
 
-ls -la /github/workspace
+#flutter test
+flutter test --machine |& tee test-result.txt
 
-ls /app/flutter-autograding
+#install python3
+sudo apt install python3
 
+#move test log files to autograding folder
 cp /github/workspace/test-result.txt /app/flutter-autograding/test-result.txt
 
-ls /app/flutter-autograding
-
+#run parser to generate summative and formative feedback
 python3 /app/flutter-autograding/main.py /app/flutter-autograding/test-result.txt $GITHUB_ACTOR $GITHUB_REPOSITORY $GITHUB_ACTION
