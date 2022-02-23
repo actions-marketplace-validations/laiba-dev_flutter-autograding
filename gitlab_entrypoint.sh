c9e0761cf71c5814ada09ade04d65247c47e4ae3
mirfanrafif17 <mirfanrafif17@gmail.com>
2022-02-23 12:06:09 +0700

    docs

--- gitlab_entrypoint.sh
+++ gitlab_entrypoint.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
 
+#ambil token github apps. anggap aja url itu buat ngambil token lah ya. nanti dipindah ke url asli
+echo "Getting Access Token from Github"
+result=$(wget -qO- http://168.138.160.59/api/access_token --header "Authorization: Bearer $API_TOKEN")
+
+#ambil test file
+echo "Cloning Test Files"
+git clone https://$result@github.com/laiba-dev/hello-flutter-test.git test
+
 #config flutter
 export PATH="$PATH":"$HOME/.pub-cache/bin"
+echo "Getting Dependencies for Flutter"
 flutter pub get
 
 #flutter test
-flutter test --machine |& tee test-result.txt
-
-#install python3
-sudo apt install python3
+echo "Running Flutter Test"
+flutter test --machine 2>test-runtime-errors.txt 1>test-result.txt
 
 #move test log to flutter autogradign folder
 cp ./test-result.txt /app/flutter-autograding/test-result.txt
 cd /app/flutter-autograding
 
-#run parser to generate and send summative and formative feedback
-python3 main.py ./test-result.txt $GITLAB_USER_LOGIN $CI_PROJECT_NAME $CI_PIPELINE_IID
+#run parser to generate summative and formative feedback
+echo "Running Parser"
+python3 main.py ./test-result.txt ./test-runtime-errors.txt $GITLAB_USER_LOGIN $CI_PROJECT_NAME $CI_PIPELINE_IID
